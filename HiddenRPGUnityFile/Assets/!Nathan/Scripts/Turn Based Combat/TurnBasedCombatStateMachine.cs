01fe059df4ddc213300e8a6f970fdc195fb834b5
Nathan Weis <17.nathan.weis@ksd.org>
2018-05-08 13:55:48 -0700

    fix plz

---
+++ HiddenRPGUnityFile/Assets/!Nathan/Scripts/Turn Based Combat/TurnBasedCombatStateMachine.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TurnBasedCombatStateMachine : MonoBehaviour {
+
+
+    public enum BattleStates
+    {
+        START,
+        PLAYERCHOICE,
+        ENEMYCHOICE,
+        CALCULATEDAMAGE,
+        LOSE,
+        WIN
+    }
+
+    public static BattleStates currentState;
+
+    public void SpawnEnemies()
+    {
+
+    }
+
+    void Start()
+    {
+        currentState = BattleStates.START;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+
+            case (BattleStates.START):
+
+
+
+                break;
+
+
+            case (BattleStates.PLAYERCHOICE):
+
+                break;
+
+
+            case (BattleStates.ENEMYCHOICE):
+
+                break;
+
+
+            case (BattleStates.CALCULATEDAMAGE):
+
+                break;
+
+
+            case (BattleStates.LOSE):
+
+                break;
+
+
+            case (BattleStates.WIN):
+
+                break;
+
+        }
+    }
+}
