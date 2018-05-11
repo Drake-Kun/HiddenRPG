db1475534b6d3094a68d181452d229a522b913e1
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-05-11 11:40:00 -0700

    ikd

--- HiddenRPGUnityFile/Assets/prototyping/combatidea.cs
+++ HiddenRPGUnityFile/Assets/prototyping/combatidea.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class combatidea : ScriptableObject {
     public string Name;
     public Sprite sprite;
+    public int mana;
+    public int maxhp;
     public int hp;
     public int lv;
     public int exp;
     public int expNeeded;
-    int exponent = 900;
+    public int exponent = 900;
     public int str;
     public int speed;
     public int intel;
     public int def;
     public int magdef;
+    public int hpscale;
     public bool prof_str;
     public bool prof_speed;
     public bool prof_intel;
     public bool prof_def;
     public bool prof_magdef;
     public string[] spellbook;
+
     public void AfterBattle()
     {
 
@@ -30,35 +34,43 @@ public class combatidea : ScriptableObject {
             lv++;
             expNeeded += exponent;
             exponent += 300;
+            if(hpscale <= 4)
+            {
+                maxhp += hpscale;
+            }
+            if (hpscale <= 4)
+            {
+                maxhp += hpscale;
+            }
             if (!prof_str)
-                str += str + 3 +Mathf.RoundToInt(lv * .2f);
+                str = str + 3 +Mathf.RoundToInt(lv * .2f);
             else if (prof_str)
             {
-                str += str + 6;
+                str = str + 6 + Mathf.RoundToInt(lv * .25f);
             }
             if (!prof_speed)
-                speed += speed + 3 + Mathf.RoundToInt(lv * .2f);
+                speed = speed + 3 + Mathf.RoundToInt(lv * .2f);
             else if (prof_speed)
             {
-                speed += speed + 6;
+                speed = speed + 6 + Mathf.RoundToInt(lv * .25f);
             }
             if (!prof_intel)
-                intel += intel + 3 + Mathf.RoundToInt(lv * .2f);
+                intel = intel + 3 + Mathf.RoundToInt(lv * .2f);
             else if (prof_intel)
             {
-                intel += intel + 6;
+                intel = intel + 6 + Mathf.RoundToInt(lv * .25f);
             }
             if (!prof_def)
-                def += def + 3 + Mathf.RoundToInt(lv * .2f);
+                def = def + 3 + Mathf.RoundToInt(lv * .2f);
             else if (prof_def)
             {
-                def += def + 6;
+                def = def + 6 + Mathf.RoundToInt(lv * .25f);
             }
             if (!prof_magdef)
-                magdef += magdef + 3 + Mathf.RoundToInt(lv * .2f);
+                magdef = magdef + 3 + Mathf.RoundToInt(lv * .2f);
             else if (prof_magdef)
             {
-                magdef += magdef + 6;
+                magdef = magdef + 6 + Mathf.RoundToInt(lv * .25f);
             }
         }
         // add exp
