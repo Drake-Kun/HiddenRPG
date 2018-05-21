3090257b41661c7b3eda886cca90df15244f8e9c
Gerardo Gallegos <18.gerardo.gallegos@ksd.org>
2018-05-21 11:39:30 -0700

    Unit

---
+++ HiddenRPGUnityFile/Assets/Gerardo/BackToTown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToTown : MonoBehaviour {
+
+	/*we created a new function every object with this script attached to it
+	 * collides with another object this function recieves a variable of type collision 2d named myCollisionInfo that acts like and incident
+	 */
+	void OnCollisionEnter2D(Collision2D myCollisionInfo)
+	{
+		//we debug.log the name of the object we
+		//ran into,that triggered the collision
+		Debug.Log (myCollisionInfo.gameObject.name); 
+		if (myCollisionInfo.gameObject.name == "Door") 
+		{
+			//load next level
+			SceneManager.LoadScene("Town1");
+			//this code will reload the current scene
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
+		}
+	}
+
+}
