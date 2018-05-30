4d46777844a7bb2978ed7012cffbb204b2b76131
Gerardo Gallegos <18.gerardo.gallegos@ksd.org>
2018-05-30 13:42:14 -0700

    Unit

---
+++ HiddenRPGUnityFile/Assets/Gerardo/Gerardo's Scripts/Leaving.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Leaving : MonoBehaviour {
+
+	/*we created a new function for every object with this script attached to it
+	 * collides with another object this function recieves a variable of type collision 2d named myCollisionInfo that acts like and incident
+	 */
+	void OnCollisionEnter2D(Collision2D myCollisionInfo)
+	{
+		//we debug.log the name of the object we
+		//ran into,that triggered the collision
+		Debug.Log (myCollisionInfo.gameObject.name); 
+		if (myCollisionInfo.gameObject.name == "Exit") 
+		{
+			//load next level
+			SceneManager.LoadScene("Town1");
+			//this code will reload the current scene
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
+		}
+	}
+
+}
