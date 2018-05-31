d9331d12254fa919c74ad092d66dad20d66120c4
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-05-31 12:14:06 -0700

    a

--- HiddenRPGUnityFile/Assets/prototyping/TargetButtons.cs
+++ HiddenRPGUnityFile/Assets/prototyping/TargetButtons.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TargetButtons : MonoBehaviour {
     public GameObject Enemy;
     public int num;
-	// Use this for initialization
+    // Use this for initialization
+    public void ModifiedStart()
+        {
+           Enemy = GameObject.Find("combat machine").GetComponent<TurnBasedCombatStateMachine>().enemyUnits[num];
+
+        }
 	void FixedUpdate () {
 
-        if (Enemy == null)
+/*        if (Enemy == null)
         {
             Enemy = GameObject.Find("combat machine").GetComponent<TurnBasedCombatStateMachine>().enemyUnits[num];
-        }
+           */ 
+            if (Enemy != null)
+            {
+                gameObject.GetComponent<Button>().interactable = true;
+                gameObject.GetComponentInChildren<Text>().text = Enemy.GetComponent<EnemyInfo>().name;
+            }
+            //}
+        else if (Enemy == null)
+            gameObject.GetComponent<Button>().interactable = false;
 	}
 	
 	// Update is called once per frame
