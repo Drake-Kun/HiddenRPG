e525c5ffff2db16233c4ecf3a425fe358d744a0c
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-06-04 13:25:06 -0700

    aaa

---
+++ HiddenRPGUnityFile/Assets/prototyping/Pause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    public GameObject Canvas;
+    public string button;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(button))
+        {
+            Switch();
+        }
+	}
+
+    void Switch()
+    {
+        if (Canvas.activeSelf)
+        {
+            Canvas.SetActive(false);
+        }
+        else
+            Canvas.SetActive(true);
+    }
+}
